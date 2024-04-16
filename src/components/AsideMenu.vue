5e01edbb630b17ebe4ed970d04dc488210e4a1e0
seimonik <simonik1002@mail.ru>
2024-04-16 22:54:18 +0400

    add aside menu

---
+++ src/components/AsideMenu.vue
@@ -0,0 +1,16 @@
+<template>
+  <el-menu default-active="2" class="el-menu-vertical-demo">
+    <el-menu-item index="1">
+      <el-icon><Edit /></el-icon>
+      <span>Документы</span>
+    </el-menu-item>
+    <el-menu-item index="2">
+      <el-icon><icon-menu /></el-icon>
+      <span>Navigator Two</span>
+    </el-menu-item>
+    <el-menu-item index="3">
+      <el-icon><document /></el-icon>
+      <span>Расчет часов</span>
+    </el-menu-item>
+  </el-menu>
+</template>
---
+++ src/views/MainView.vue
@@ -0,0 +1,14 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-aside width="250px">
+        <AsideMenu />
+      </el-aside>
+      <el-main><router-view /></el-main>
+    </el-container>
+  </div>
+</template>
+<script setup>
+import AsideMenu from "../components/AsideMenu.vue";
+</script>
